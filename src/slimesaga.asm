2072c3524759be5dcac367ae1425427b899272a4
chickendude <pokemaster103@gmail.com>
2023-08-09 20:41:01 -0400

    Start scrolling implementation

--- src/slimesaga.asm
+++ src/slimesaga.asm
@@ -1,8 +1,23 @@
 include "inc/hardware.inc"
 
-def HEIGHT_T equ 9
-def WIDTH_T equ 10
-def map_w equ _HRAM
+def HEIGHT_T	equ 9
+def WIDTH_T		equ 10
+
+; high RAM: ~126 bytes from $FF80 - $FFFE
+def map_w_extra	equ _HRAM ; how much to add to move down a row in the map
+
+
+; RAM: ~8000 bytes from $C000 - $DFFF
+section "variables", wram0[_RAM]
+START_OF_VARIABLES:
+map_w: DS 1
+map_h: DS 1
+map_x: DS 2
+map_y: DS 2
+player_x: DS 2
+player_y: DS 2
+END_OF_VARIABLES:
+
 
 section "header", rom0[$0100]
 entrypoint:
@@ -13,15 +28,25 @@ entrypoint:
 
 section "main", rom0[$0150]
 start:
-	ld a, [rLY]
+	ld a, [rLY]			; wait for VBLANK to turn off LCD
 	cp SCRN_Y
 	 jr nz, start
 	xor a
-	ld [rLCDC], a
+	ld [rLCDC], a		; disable LCD
 
-	ld a, %11100100 ; light to dark
-	ld [rBGP], a	; update palette
+	ld a, %11100100 	; light to dark
+	ld [rBGP], a		; update palette
 
+; set up RAM variables
+	ld hl, map_w
+	ld b, END_OF_VARIABLES - START_OF_VARIABLES
+	xor a
+.reset:
+	ld [hl+], a
+	dec b
+	 jr nz, .reset
+
+; load tile data into VRAM
 	ld de, tile_data
 	ld hl, _VRAM8000
 	ld b, 16
@@ -48,6 +73,8 @@ main:
 	ld a, [rLY]
 	cp SCRN_Y
 	 jr nz, .vblank
+	
+	call update_camera	; [camera.asm]
 
 	ld a, P1F_GET_DPAD	; prepare to read DPAD from key port
 	ld [rP1], a			; send request to read DPAD status
@@ -57,19 +84,19 @@ main:
 	swap a
 	push af
 		and PADF_UP			;
-		 call z, move_up
+		 call z, move_up	; [tilemap.asm]
 	pop af
 	push af
 		and PADF_DOWN		;
-		 call z, move_down
+		 call z, move_down	; [tilemap.asm]
 	pop af
 	push af
 		and PADF_LEFT		;
-		 call z, move_left
+		 call z, move_left	; [tilemap.asm]
 	pop af
 	push af
 		and PADF_RIGHT		;
-		 call z, move_right
+		 call z, move_right	; [tilemap.asm]
 	pop af
 
 	ld a, P1F_GET_NONE	; disable key polling
@@ -78,23 +105,7 @@ main:
 
 ; ### CODE ###
 
-move_up:
-	ld hl, rSCY
-	dec [hl]
-	ret
-move_right:
-	ld hl, rSCX
-	inc [hl]
-	ret
-move_down:
-	ld hl, rSCY
-	inc [hl]
-	ret
-move_left:
-	ld hl, rSCX
-	dec [hl]
-	ret
-
+include "camera.asm"
 include "tilemap.asm"
 
 ; ### DATA ###
@@ -104,15 +115,15 @@ incbin "gfx/tiles.bin"
 tile_end:
 
 tilemap:
-db 12
-db 0, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2
-db 2, 0, 0, 2, 2, 0, 0, 0, 1, 2, 1, 2
-db 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 1, 2
-db 0, 0, 0, 2, 0, 0, 0, 2, 2, 2, 1, 2
-db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2
-db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2
-db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2
-db 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 1, 2
-db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2
-db 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1, 2
+db 13
+db 0, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2
+db 2, 0, 0, 2, 2, 0, 0, 0, 1, 2, 0, 2, 2
+db 0, 0, 0, 2, 2, 0, 0, 0, 2, 2, 0, 0, 0
+db 0, 0, 0, 2, 0, 0, 0, 2, 2, 2, 0, 0, 2
+db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2
+db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2
+db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2
+db 0, 0, 0, 0, 1, 0, 0, 0, 0, 2, 2, 0, 2
+db 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 1, 2
+db 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2
 db $FF
--- src/tilemap.asm
+++ src/tilemap.asm
@@ -4,8 +4,9 @@ load_tilemap:
 	ld hl, _SCRN0
 	ld c, HEIGHT_T + 1
 	ld a, [de]			; first byte in tilemap holds map width
+	ld [map_w], a
 	sub a, WIDTH_T + 1
-	ldh [map_w], a
+	ldh [map_w_extra], a
 .row:
 	ld b, WIDTH_T + 1	; 10 16x16 tiles wide (160 pixels)
 .draw_tile:
@@ -25,7 +26,7 @@ load_tilemap:
 		ld [hl], a		; byte 4
 	pop bc				; bc = loop counters
 	pop hl				; hl = SCRN
-	inc hl				; shift to next 16x16 tile in tilemap
+	inc hl				; hl + 2: shift to next 16x16 tile in tilemap
 	inc hl				;
 	dec b				; check if full row has been drawn
 	 jr nz, .draw_tile	; .. if not, draw next tile
@@ -37,7 +38,7 @@ load_tilemap:
 		push hl			; hl = SCRN
 			push de		; ex de, hl
 			pop hl		; hl = tilemap position, de = SCRN
-			ldh a, [map_w]	; load map width into bc
+			ldh a, [map_w_extra]	; load map width into bc
 			ld c, a		; b should still be zero
 			add hl, bc	; add map width to tilemap (move down one row in tilemap)
 			ld e, l		; de = new tilemap position
@@ -45,4 +46,94 @@ load_tilemap:
 		pop hl			; hl = SCRN
 	pop bc				; bc = row + column counters
 	jr .row
+
+; a = player_x
+draw_column:
+	ld bc, HEIGHT_T + 1	; c = number of rows to draw
+	rra					; /2
+	rra					; /4
+	rra					; /8
+	rra					; /16
+	and %00011111		; clear out shifted bits
+	ld e, a				; de = x position
+	ld d, b				; (b set to 0 above)
+	ld hl, tilemap + WIDTH_T + 1	; right side of tilemap
+	add hl, de			; first tile in column
+	; TODO: add y position
+	push hl
+		ld hl, _SCRN0 + 20
+		add hl, de
+		add hl, de
+	pop de				; hl = VRAM tilemap, de = local tilemap
+.loop:
+	push bc				; save row counter (c)
+		ld a, [de]		; a = tilemap tile
+		add a, a		; x2
+		ld [hl+], a		; save BYTE 1
+		inc a			; 
+		ld [hl], a		; BYTE 2
+		add a, $F		; currently, tiles are stored in 2D, so we need to shift down a row
+		ld c, 31		; bc = 31 (shift down a row in SCRN0)
+		add hl, bc		; go to next row in SCRN0
+		ld [hl+], a		; BYTE 3
+		inc a			;
+		ld [hl], a		; BYTE 4
+		add hl,bc
+		push hl
+			ld l, e		; ld hl, de = position in tilemap
+			ld h, d		;
+			ld a, [map_w]	;
+			ld c, a		; bc = map width
+			add hl, bc	; go to next row in tilemap
+			ld e, l		; ld de, hl
+			ld d, h		; .. save result back to de (new position in tilemap)
+		pop hl
+	pop bc
+	dec c
+	 jr nz, .loop
+	ret
+	
+
+move_up:
+	;ld hl, rSCY
+	ld hl, player_y
+	ld a, [hl]
+	or a
+	 ret z
+	dec [hl]
+	ret
+move_left:
+	;ld hl, rSCX
+	ld hl, player_x
+	ld a, [hl]
+	or a
+	 ret z
+	dec [hl]
+	ret
+move_right:
+	;ld hl, rSCX
+	ld hl, player_x
+	ld a, [hl]			; a = player's x position
+	inc a				; check x position to the right one pixel
+	rra					; /2 tiles are 16 pixels wide
+	rra					; /4
+	rra					; /8
+	rra					; /16
+	and %00001111		; clear out possible carries
+	add a, WIDTH_T		; check right edge of the screen
+	ld b, a				;
+	ld a, [map_w]		; check if x position has reached right edge of screen
+	cp b				; if so, don't move player any further
+	 ret z				; 
+	inc [hl]			; move player right one pixel
+	ld a, [hl]			; check if we've passed a tile boundary
+	and $0F				;
+	 ret nz				; if not, we're done
+	ld a, [hl]			; a = new player x
+	jr draw_column		; draw a new column
+move_down:
+	;ld hl, rSCY
+	ld hl, player_y
+	inc [hl]
+	ret
 
