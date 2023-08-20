1d41cabdf34b6b7ace152a7f0bf87ea95cf936ec
chickendude <pokemaster103@gmail.com>
2023-08-20 17:06:11 -0400

    Handle vertical and horizontal scrolling

--- src/tilemap.asm
+++ src/tilemap.asm
@@ -66,7 +66,7 @@ mult_he:
 .loop:
 	add hl, hl          ; if there was a carry, we need to add e
      jr nc, .skip
-		add	hl, de
+		add hl, de
 .skip: 
 	dec b				; repeat 8 times
      jr nz, .loop
@@ -104,7 +104,7 @@ draw_column:
 	rla
 	swap a
 	ld c, a
-	and %1110_0000
+	and %1100_0000
 	add a, l
 	ld l, a
 	adc a, h
@@ -134,6 +134,7 @@ draw_column:
 	ld l, a				; l = l + 31
 	adc a, h			; add carry over from l to h
 	sub l				; subtract l (because we only wanted to add the carry)
+	and %1111_1011
 	ld h, a				; hl = hl + 31, SCRN0 shifted down a row
 ; shift de (tilemap) down a row
 	ld a, [map_w]		; add hl, 31: shift down another row
@@ -150,14 +151,14 @@ move_left:
 	ld hl, map_x		; first check if we're at the edge of the map
 	ld a, [hl+]			; pixel offset + subpixel offset
 	and $F0				; clear out subpixel offset (12.4 fixed point)
-	cp [hl]				; check if both the tile and pixel offset are zero
+	or [hl]				; check if both the tile and pixel offset are zero
 	 ret z				; .. if so, can't move left any further
 ; update x position
 	dec hl				; LSB of map_w
 	ld a, [hl]			; subtract walking speed from map_x
 	sub a, SPEED		; 
 	ld [hl+], a
-	 jr nc, .skip		; if there was no carry, no need to update the MSB of map_x
+	 ret nc				; if there was no carry, no need to update the MSB of map_x
 		dec [hl]
 .skip:
 	ld a, [hl]
@@ -173,45 +174,110 @@ move_right:
 	dec hl				; LSB of map_w
 	ld a, [hl]			; increase map_x by one, making sure to carry over
 	add a, SPEED		; 
-	ld c, a
 	ld [hl+], a
-	adc a, [hl]
-	sub c
-	ld [hl], a
-	add a, WIDTH_T + 1
+	 ret nc
+		inc [hl]
+	ld a, [hl]
+	add a, WIDTH_T
 	jr draw_column		; draw a new column
 
 move_up:
 	ld hl, map_y		; check if we're already at the top of the map
 	ld a, [hl+]			; a = LSB
 	and $F0				; clear out the subpixel offset
-	cp [hl]				; if LSB and MSB both = 0, we're at the top
+	or [hl]				; if LSB and MSB both = 0, we're at the top
 	 ret z				; quit if so
 ; update y position
-	dec hl
+	dec hl				; hl = LSB of map_y
 	ld a, [hl]			; subtract walking speed from map_y
 	sub a, SPEED		; 
-	ld [hl+], a
-	 jr nc, .skip		; if there was no carry, no need to update the MSB of map_y
-		dec [hl]
+	ld [hl+], a			; save new position
+	 ret nc				; if there was no carry, no need to update the MSB of map_y
+		dec [hl]		; otherwise, update MSB of map_y
+		 ;ret z			; if we're at the top, no need to draw the row above
 .skip:
-	ret
+	ld a, [hl]			; a = new map_y tile
+	;dec a				; a = row above current row
+	jr draw_row			; draw the row just offscreen above the current row
 
 move_down:
-	ld hl, map_y + 1
-	ld a, [map_h]
-	sub HEIGHT_T
-	cp [hl]
-	 ret z
-; updaty y position
-	dec hl
-
-	ld a, [hl]
-	add a, SPEED
-	ld c, a
+	ld hl, map_y + 1	; MSB of map_y (aligned tile position)
+	ld a, [map_h]		; check if at the bottom of the map
+	sub HEIGHT_T		; adjust a so that we can compare it to the map y tile position
+	cp [hl]				; map_y == map_h - num_rows
+	 ret z				; quit if we're at the bottom
+; update y position
+	dec hl				; LSB of map_y
+	ld a, [hl]			; 
+	add a, SPEED		; add speed to map_y
 	ld [hl+], a
-	adc a, [hl]
-	sub c
-	ld [hl], a
-	ret
+	 ret nc
+	inc [hl]
+.skip
+	ld a, [hl]
+	add a, HEIGHT_T
+; continue into draw_row
 
+; a = row to draw
+draw_row:
+	ld c, a
+; calculate starting position in tilemap
+	ld h, a
+	ld a, [map_w]
+	ld e, a
+	call mult_he
+	ld de, tilemap + 2
+	ld a, [map_x + 1]
+	add a, e
+	ld e, a
+	adc a, d
+	sub e
+	ld d, a
+	add hl, de
+	ld e, l
+	ld d, h
+; calculate position in SCRN0
+	ld a, c
+	swap a
+	rlca
+	rlca
+	ld l, a
+	and %0000_0011
+	add a, high(_SCRN0)
+	ld h, a
+	ld a, l
+	and %1100_0000
+	ld l, a
+	ld a, [map_x + 1]
+	and $F
+	add a, a
+	add a, l
+	ld l, a
+	adc a, h
+	sub l
+	ld h, a
+	ld a, l
+	and %1110_0000
+	ld b, a
+	ld c, WIDTH_T + 1
+.loop:
+	ld a, [de]			; a = tilemap tile
+	add a, a			; x2
+	add a, a			; x4 (16x16 tiles = 4 8x8 tiles)
+	ld [hl+], a			; set BYTE 1
+	inc a				; 
+	ld [hl-], a			; BYTE 2
+	set 5, l			; + 32, even to odd row
+	inc a				; 
+	ld [hl+], a			; BYTE 3
+	inc a				;
+	ld [hl], a			; BYTE 4
+	ld a, l
+	inc a
+	and %0001_1111
+	or b
+	ld l, a
+	inc de				; next tile in tilemap data
+	dec c
+	 jr nz, .loop
+	ret
